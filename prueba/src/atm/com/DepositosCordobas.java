f73c123ffff1c89ed90413dcd60ec3b4e9e12b72
arsu28 <arsu28@gmail.com>
2023-03-24 16:06:50 -0600

    Agregando clase depositos en cordobas

---
+++ prueba/src/atm/com/DepositosCordobas.java
@@ -0,0 +1,9 @@
+package atm.com;
+
+public class DepositosCordobas {
+    public double sumarDepositoCordobas (double saldoInicial, double monto) {
+        double saldoFinal;
+            saldoFinal = saldoInicial + monto;
+            return saldoFinal;
+    }
+}
